54152665024970c7b7ed4ca3e1fd6185cf9248c4
brad pizzimenti and tyler suderman <brad.pizzimenti@gmail.com>
2016-02-08 09:44:01 -0800

    initial commit with first passing test

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,8 @@
+public class PingPong {
+  public static void main(String[] args) {}
+
+    public Boolean isPingPong(Integer number) {
+      //code for testing fifteen even divisibility
+      return number % 15 == 0;
+    }
+}
